f2cb38f63fb96999277ae588f1d13f24a9f25682
SweetCoding115 <moriahbuckridge96115@gmail.com>
2023-11-29 20:53:08 +0800

    Post table, model, controller and routes generated

--- database/migrations/2023_11_29_114005_create_posts_table.php
+++ database/migrations/2023_11_29_114005_create_posts_table.php
@@ -9,10 +9,12 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// returns the home page with all posts
+Route::get('/', PostController::class .'@index')->name('posts.index');
+// returns the form for adding a post
+Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+// adds a post to the database
+Route::post('/posts', PostController::class .'@store')->name('posts.store');
+// returns a page that shows a full post
+Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
+// returns the form for editing a post
+Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
+// updates a post
+Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
+// deletes a post
+Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
